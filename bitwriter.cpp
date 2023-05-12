5492fcfbab3dd7f90b83f3c62b50fa605c467c3b
Mattia Gualtieri <37586010+mattiagualtieri@users.noreply.github.com>
2023-05-12 19:02:25 +0200

    added bitwriter

---
+++ bitwriter.cpp
@@ -0,0 +1,44 @@
+#include <fstream>
+
+template<typename T>
+std::ostream& raw_write(std::ostream& os, const T& val, size_t size = sizeof(T)) {
+	return os.write(reinterpret_cast<const char*>(&val), sizeof(val));
+}
+
+class bitwriter {
+	uint8_t buffer;
+	int n = 0;
+	std::ostream& os;
+
+	std::ostream& write_bit(uint8_t bit) {
+		buffer = (buffer << 1) | (bit & 1);
+		n++;
+		if (n == 8) {
+			raw_write(os, buffer);
+			n = 0;
+		}
+		return os;
+	}
+
+public: 
+	bitwriter(std::ostream& os) : os(os) {}
+
+	std::ostream& write(uint32_t u, uint8_t n) {
+		for (int i = n - 1; i >= 0; i--) {
+			write_bit(u >> i);
+		}
+		return os;
+	}
+
+	std::ostream& flush(uint32_t bit = 0) {
+		while (n > 0) {
+			write_bit(bit);
+		}
+		return os;
+	}
+
+	~bitwriter() {
+		flush();
+	}
+
+};
