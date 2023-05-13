98e44ae8ee323e4e3dccc94f6e252a258180d3b5
Mattia Gualtieri <37586010+mattiagualtieri@users.noreply.github.com>
2023-05-13 21:32:54 +0200

    added frequency_counter (an example of std::map)

---
+++ frequency_counter.cpp
@@ -0,0 +1,22 @@
+#include <map>
+
+class frequency_counter {
+	map<uint8_t, int> freqs;
+
+public:
+	map<uint8_t, int> load(istream& is) {
+		while (true) {
+			uint8_t byte = is.get();
+			if (!is) {
+				return freqs;
+			}
+			if (freqs.count(byte) == 0) {
+				freqs.insert({ byte, 1 });
+			}
+			else {
+				freqs.at(byte) = freqs.at(byte) + 1;
+			}
+		}
+		return freqs;
+	}
+};
