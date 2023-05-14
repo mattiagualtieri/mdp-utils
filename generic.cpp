ea04c9bb3394fcb4ce9b69157de87de032c123e8
Mattia Gualtieri <37586010+mattiagualtieri@users.noreply.github.com>
2023-05-14 17:37:41 +0200

    added method

--- generic.cpp
+++ generic.cpp
@@ -11,3 +11,13 @@ void sort(vector<T>& v) {
 		}
 	}
 }
+
+// Useful method to parse a 32 bit integer into 4 uint8_t (little endian)
+uint8_t* from_32_to_8_bits(uint32_t value) {
+	uint8_t ret[4];
+	ret[0] = (value & 0x000000ff);
+	ret[1] = (value & 0x0000ff00) >> 8;
+	ret[2] = (value & 0x00ff0000) >> 16;
+	ret[3] = (value & 0xff000000) >> 24;
+	return ret;
+}
