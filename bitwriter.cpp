21023c120b7cd13937afd503bce4a512470c9b96
Mattia Gualtieri <37586010+mattiagualtieri@users.noreply.github.com>
2023-06-01 17:26:02 +0200

    updated bitwriter

--- bitwriter.cpp
+++ bitwriter.cpp
@@ -1,8 +1,8 @@
 #include <fstream>
 
 template<typename T>
-std::ostream& raw_write(std::ostream& os, const T& val, size_t size = sizeof(T)) {
-	return os.write(reinterpret_cast<const char*>(&val), size);
+std::ostream& raw_write(std::ostream& os, const T& value, size_t size = sizeof(T)) {
+	return os.write(reinterpret_cast<const char*>(&value), size);
 }
 
 class bitwriter {
@@ -10,31 +10,28 @@ class bitwriter {
 	int n_ = 0;
 	std::ostream& os_;
 
-	std::ostream& write_bit(uint32_t bit) {
+	void write_bit(uint32_t bit) {
 		buffer_ = (buffer_ << 1) | (bit & 1);
 		n_++;
 		if (n_ == 8) {
 			raw_write(os_, buffer_);
 			n_ = 0;
 		}
-		return os_;
 	}
 
 public:
 	bitwriter(std::ostream& os) : os_(os) {}
 
-	std::ostream& write(uint8_t u, size_t n) {
-		while (n --> 0) {
-			write_bit(u >> n);
+	void write(uint32_t u, uint8_t n) {
+		for (int i = n - 1; i >= 0; --i) {
+			write_bit(u >> i);
 		}
-		return os_;
 	}
 
-	std::ostream& flush(uint32_t bit = 0) {
+	void flush(uint32_t bit = 0) {
 		while (n_ > 0) {
 			write_bit(bit);
 		}
-		return os_;
 	}
 
 	~bitwriter() {
